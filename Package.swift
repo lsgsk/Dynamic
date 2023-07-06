c20c597caaedc9e9b3aa9b30199df4c4de90001d
lsgsk <lsgsk@rambler.ru>
2023-07-06 16:55:07 +0300

    Осилил черепашку

--- Package.swift
+++ Package.swift
@@ -8,12 +8,16 @@ let package = Package(
         .executable(name: "Part1.1", targets: ["Part1.1"]),
         .executable(name: "Part1.2", targets: ["Part1.2"]),
         .executable(name: "Part1.3", targets: ["Part1.3"]),
+        .executable(name: "Part1.4", targets: ["Part1.4"]),
     ],
+    dependencies: [],
     targets: [
         .target(name: "Matrix"),
         .executableTarget(name: "Part1.1"),
         .executableTarget(name: "Part1.2"),
         .executableTarget(name: "Part1.3", dependencies: ["Matrix"]),
         .testTarget(name: "Tests1.3", dependencies: ["Part1.3", "Matrix"]),
+        .executableTarget(name: "Part1.4", dependencies: ["Matrix"]),
+        .testTarget(name: "Tests1.4", dependencies: ["Part1.4", "Matrix"]),
     ]
 )
